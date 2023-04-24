dc7f906a52013d0a6afc99dddb5470a4d66db25c
礒邉大輝 <k022g1059@g.neec.ac.jp>
2023-04-24 15:12:34 +0900

    確認課題の作業中

---
+++ Player.cpp
@@ -0,0 +1,19 @@
+#include <Player.h>
+#include <assert.h>
+
+void Player::Initialize(Model* model,uint32_t textureHandle) { 
+	assert(model);
+
+	model = model_;
+	textureHandle = textureHandle_;
+
+
+}
+
+void Player::Update() {
+
+}
+
+void Player::Draw() {
+
+}
--- Player.h
+++ Player.h
@@ -1,1 +1,20 @@
 #pragma once
+#include<Model.h>
+#include<WorldTransform.h>
+
+class Player {
+public:
+	void Initialize(Model* model,uint32_t textureHandle);
+
+	void Update();
+
+	void Draw();
+
+private:
+	WorldTransform worldTransform_;
+
+	Model* model_ = nullptr;
+
+	uint32_t textureHandle_ = 0u;
+
+};
