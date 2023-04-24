ef1ef4c336d1ecd5a00b0d46ddb8d411b22d112a
礒邉大輝 <k022g1059@g.neec.ac.jp>
2023-04-24 14:43:02 +0900

    確認課題作業中

--- scene/GameScene.cpp
+++ scene/GameScene.cpp
@@ -2,7 +2,7 @@
 #include "TextureManager.h"
 #include <cassert>
 
-GameScene::GameScene() {}
+GameScene::GameScene() { delete model_, delete player_; }
 
 GameScene::~GameScene() {}
 
@@ -11,9 +11,16 @@ void GameScene::Initialize() {
 	dxCommon_ = DirectXCommon::GetInstance();
 	input_ = Input::GetInstance();
 	audio_ = Audio::GetInstance();
+	textureHandle_ = TextureManager::Load("sample.png");
+	model_ = Model::Create();
+	viewProjection_.Initialize();
+	player_ = new Player();
+	player_->Initialize();
+
 }
 
-void GameScene::Update() {}
+void GameScene::Update() { 
+	player_->Update(); }
 
 void GameScene::Draw() {
 
@@ -41,6 +48,7 @@ void GameScene::Draw() {
 	/// <summary>
 	/// ここに3Dオブジェクトの描画処理を追加できる
 	/// </summary>
+	player_->Draw();
 
 	// 3Dオブジェクト描画後処理
 	Model::PostDraw();
