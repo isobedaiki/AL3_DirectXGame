6f4a6e0fa5179aa7ea10e282669e60e2ec279a6f
礒邉大輝 <k022g1059@g.neec.ac.jp>
2023-05-19 14:32:35 +0900

    02_07_確認課題

--- Enemy.cpp
+++ Enemy.cpp
@@ -9,7 +9,20 @@ void Enemy::Initialize(Model* model, const Vector3& pos) {
 void Enemy::Update() { 
 	const float kcharacterSpeed = 0.3f;
 	worldTransform_.UpdateMatrix();
-	worldTransform_.translation_.z -= kcharacterSpeed;
+	//worldTransform_.translation_.z -= kcharacterSpeed;
+
+	switch (phase_) {
+	case Phase::Approach:
+	default:
+		worldTransform_.translation_.z -= kcharacterSpeed;
+		if (worldTransform_.translation_.z < 0.0f) {
+			phase_ = Phase::Leave;
+		}
+		break;
+	case Phase::Leave:
+		worldTransform_.translation_.z += kcharacterSpeed;
+		break;
+	}
 
 }
 void Enemy::Draw(ViewProjection& viewProjection) {
--- Enemy.h
+++ Enemy.h
@@ -4,15 +4,23 @@
 #include "ViewProjection.h"
 #include"WorldTransform.h"
 
+enum class Phase {
+	Approach,
+	Leave, 
+};
+
 class Enemy {
 public:
 	void Initialize(Model* model, const Vector3& pos);
 	void Update();
 	void Draw(ViewProjection& viewProjection);
-
+	Phase phase_ = Phase::Approach;
+	
 private:
 	WorldTransform worldTransform_;
 	uint32_t textureHandle_ = 0u;
 	Model* model_ = nullptr;
 	Input* input_ = nullptr;
+	//Phase* phase_ = nullptr;
 };
+
